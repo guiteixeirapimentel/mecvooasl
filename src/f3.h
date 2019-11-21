6ca956ae770328fec7214da093afc8cb8a9bc7f0
Guilherme Teixeira Pimentel <gui-teixeirapimentel@hotmail.com>
2019-11-20 22:42:10 -0300

    Corrected "mass" to "momentInertia"

--- src/f3.h
+++ src/f3.h
@@ -27,123 +27,123 @@ public:
     }
 
     double JacobianX1(double meanchord,
-    const State& Xref, const StateDot& Xdotref, double massref, double deltaEref) const
+    const State& Xref, const StateDot& Xdotref, double momentInertia, double deltaEref) const
     {
         double delta = 1e-7;
         State X = Xref;
         
         X.u = Xref.u - delta;
-        double res = operator()(meanchord, Xref, Xdotref, massref, deltaEref)- 
-        operator()(meanchord, X, Xdotref, massref, deltaEref);
+        double res = operator()(meanchord, Xref, Xdotref, momentInertia, deltaEref)- 
+        operator()(meanchord, X, Xdotref, momentInertia, deltaEref);
         res /= delta;  
 
         return res;
     }
 
     double JacobianX2(double meanchord,
-    const State& Xref, const StateDot& Xdotref, double massref, double deltaEref) const
+    const State& Xref, const StateDot& Xdotref, double momentInertia, double deltaEref) const
     {
         double delta = 1e-7;
         State X = Xref;
         
         X.w = Xref.w - delta;
-        double res = operator()(meanchord, Xref, Xdotref, massref, deltaEref)- 
-        operator()(meanchord, X, Xdotref, massref, deltaEref);
+        double res = operator()(meanchord, Xref, Xdotref, momentInertia, deltaEref)- 
+        operator()(meanchord, X, Xdotref, momentInertia, deltaEref);
         res /= delta;  
 
         return res;
     }
 
     double JacobianX3(double meanchord,
-    const State& Xref, const StateDot& Xdotref, double massref, double deltaEref) const
+    const State& Xref, const StateDot& Xdotref, double momentInertia, double deltaEref) const
     {
         double delta = 1e-7;
         State X = Xref;
         
         X.q = Xref.q - delta;
-        double res = operator()(meanchord, Xref, Xdotref, massref, deltaEref)- 
-        operator()(meanchord, X, Xdotref, massref, deltaEref);
+        double res = operator()(meanchord, Xref, Xdotref, momentInertia, deltaEref)- 
+        operator()(meanchord, X, Xdotref, momentInertia, deltaEref);
         res /= delta;  
 
         return res;
     }
 
     double JacobianX4(double meanchord,
-    const State& Xref, const StateDot& Xdotref, double massref, double deltaEref) const
+    const State& Xref, const StateDot& Xdotref, double momentInertia, double deltaEref) const
     {
         double delta = 1e-7;
         State X = Xref;
         
         X.theta = Xref.theta - delta;
-        double res = operator()(meanchord, Xref, Xdotref, massref, deltaEref)- 
-        operator()(meanchord, X, Xdotref, massref, deltaEref);
+        double res = operator()(meanchord, Xref, Xdotref, momentInertia, deltaEref)- 
+        operator()(meanchord, X, Xdotref, momentInertia, deltaEref);
         res /= delta;  
 
         return res;
     }
 
     double JacobianXdot1(double meanchord,
-    const State& Xref, const StateDot& Xdotref, double massref, double deltaEref) const
+    const State& Xref, const StateDot& Xdotref, double momentInertia, double deltaEref) const
     {
         double delta = 1e-7;
         StateDot Xdot = Xdotref;
         
         Xdot.udot = Xdotref.udot - delta;
-        double res = operator()(meanchord, Xref, Xdotref, massref, deltaEref)- 
-        operator()(meanchord, Xref, Xdot, massref, deltaEref);
+        double res = operator()(meanchord, Xref, Xdotref, momentInertia, deltaEref)- 
+        operator()(meanchord, Xref, Xdot, momentInertia, deltaEref);
         res /= delta;  
 
         return res;
     }
 
     double JacobianXdot2(double meanchord,
-    const State& Xref, const StateDot& Xdotref, double massref, double deltaEref) const
+    const State& Xref, const StateDot& Xdotref, double momentInertia, double deltaEref) const
     {
         double delta = 1e-7;
         StateDot Xdot = Xdotref;
         
         Xdot.wdot = Xdotref.wdot - delta;
-        double res = operator()(meanchord, Xref, Xdotref, massref, deltaEref)- 
-        operator()(meanchord, Xref, Xdot, massref, deltaEref);
+        double res = operator()(meanchord, Xref, Xdotref, momentInertia, deltaEref)- 
+        operator()(meanchord, Xref, Xdot, momentInertia, deltaEref);
         res /= delta;  
 
         return res;
     }
 
     double JacobianXdot3(double meanchord,
-    const State& Xref, const StateDot& Xdotref, double massref, double deltaEref) const
+    const State& Xref, const StateDot& Xdotref, double momentInertia, double deltaEref) const
     {
         double delta = 1e-7;
         StateDot Xdot = Xdotref;
         
         Xdot.qdot = Xdotref.qdot - delta;
-        double res = operator()(meanchord, Xref, Xdotref, massref, deltaEref)- 
-        operator()(meanchord, Xref, Xdot, massref, deltaEref);
+        double res = operator()(meanchord, Xref, Xdotref, momentInertia, deltaEref)- 
+        operator()(meanchord, Xref, Xdot, momentInertia, deltaEref);
         res /= delta;  
 
         return res;
     }
 
     double JacobianXdot4(double meanchord,
-    const State& Xref, const StateDot& Xdotref, double massref, double deltaEref) const
+    const State& Xref, const StateDot& Xdotref, double momentInertia, double deltaEref) const
     {
         double delta = 1e-7;
         StateDot Xdot = Xdotref;
         
         Xdot.wdot = Xdotref.wdot - delta;
-        double res = operator()(meanchord, Xref, Xdotref, massref, deltaEref)- 
-        operator()(meanchord, Xref, Xdot, massref, deltaEref);
+        double res = operator()(meanchord, Xref, Xdotref, momentInertia, deltaEref)- 
+        operator()(meanchord, Xref, Xdot, momentInertia, deltaEref);
         res /= delta;  
 
         return res;
     }
 
     double JacobianU1(double meanchord,
-    const State& Xref, const StateDot& Xdotref, double massref, double deltaEref) const
+    const State& Xref, const StateDot& Xdotref, double momentInertia, double deltaEref) const
     {
         double delta = 1e-7;        
-        double res = operator()(meanchord, Xref, Xdotref, massref, deltaEref)- 
-        operator()(meanchord, Xref, Xdotref, massref, deltaEref - delta);
+        double res = operator()(meanchord, Xref, Xdotref, momentInertia, deltaEref)- 
+        operator()(meanchord, Xref, Xdotref, momentInertia, deltaEref - delta);
         res /= delta;  
 
         return res;
